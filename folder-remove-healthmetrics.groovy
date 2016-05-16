c3e9004c23ecaa7475abf82081932b0190faaf6c
Andy Pemberton <pembertona@gmail.com>
2016-05-15 20:19:41 -0400

    align to KB article

--- folder-remove-healthmetrics.groovy
+++ folder-remove-healthmetrics.groovy
@@ -3,11 +3,10 @@ import com.cloudbees.hudson.plugins.folder.health.FolderHealthMetric
 def folders = Jenkins.instance.getAllItems(com.cloudbees.hudson.plugins.folder.Folder.class)
 folders.each{ folder ->
   folder.healthMetrics.each{ metric ->
-    if(metric.class.simpleName != "ProjectEnabledHealthMetric"){
+    if (metric instanceof com.cloudbees.hudson.plugins.folder.health.ProjectEnabledHealthMetric) {
       println "Removing ${metric.class.simpleName} from ${folder.name}"
       folder.healthMetrics.remove(metric)
     }
   }
 }
-
 return null
