193c68c434754de8d7fc4f3c775f5d107ce97558
Alex Taylor <ataylor@cloudbees.com>
2017-06-23 10:52:28 -0400

    Adding Messaging database groovy scripts

---
+++ CJP-MessagingStatus.groovy
@@ -0,0 +1,92 @@
+/*
+Author: Alvaro Lobato
+Since: June 2017
+Description: This script will print out all the inbox and outbox data from a CJE or CJOC
+instance. Use this to find out what the current messaging status is.
+Parameters: None
+Scope: Cloudbees Jenkins Operations Center or Cloudbees Jenkins Platform
+*/
+
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.SortedMap;
+
+import org.mapdb.Atomic;
+
+import com.cloudbees.opscenter.context.Messaging;
+import com.cloudbees.opscenter.context.Messaging.InboxEntry;
+import com.cloudbees.opscenter.context.Messaging.InboxKey;
+import com.cloudbees.opscenter.context.Messaging.OutboxEntry;
+
+import jenkins.model.Jenkins;
+
+
+out.println("InstanceId: " + Jenkins.getInstance().getLegacyInstanceId());
+final ClassLoader oldContext = Thread.currentThread().getContextClassLoader();
+try {
+    Thread.currentThread().setContextClassLoader(Jenkins.getInstance().getPluginManager().uberClassLoader);
+    Atomic.Long inboxSequenceId;
+    Atomic.Long outboxSequenceId;
+    SortedMap<InboxKey<?>, InboxEntry<?>> inbox;
+    SortedMap<Long, OutboxEntry<?>> outbox;
+    Messaging messaging = Messaging.getInstance();
+    Field localField;
+
+    localField = Messaging.class.getDeclaredField("local");
+    localField.setAccessible(true);
+
+    Object local = localField.get(messaging);
+    Class localClass = Messaging.class.getDeclaredClasses()[1];// 1 is the index for Messaging.Local
+
+    Field field = localClass.getDeclaredField("inbox");
+    field.setAccessible(true);
+
+    inbox = (SortedMap<InboxKey<?>, InboxEntry<?>>) field.get(local);
+
+    field = localClass.getDeclaredField("outbox");
+    field.setAccessible(true);
+    outbox = (SortedMap<Long, OutboxEntry<?>>) field.get(local);
+
+    field = localClass.getDeclaredField("inboxSequenceId");
+    field.setAccessible(true);
+    inboxSequenceId = (org.mapdb.Atomic.Long) field.get(local);
+
+    field = localClass.getDeclaredField("outboxSequenceId");
+    field.setAccessible(true);
+    outboxSequenceId = (org.mapdb.Atomic.Long) field.get(local);
+
+    try {
+        out.println("outboxSequenceId:" + outboxSequenceId);
+        out.println("outbox size: " + outbox.size());
+        Set<Entry<Long, OutboxEntry<?>>> entrySet2 = outbox.entrySet();
+        for (Entry<Long, OutboxEntry<?>> entry : entrySet2) {
+            OutboxEntry<?> value = entry.getValue();
+            out.println(entry.getKey() + " - "
+                    + "Expiry:" + new Date(value.getExpiry()) + " - "
+                    + "Destination:" + value.getDestinationId() + " - "
+                    + "Class Name:" + value.getClazzName());
+        }
+    } catch (Exception e) {
+        out.println("Could not get inbox information");
+        e.printStackTrace(out);
+    }
+    try {
+        out.println("inboxSequenceId:" + inboxSequenceId);
+        out.println("inbox size: " + inbox.size());
+        Set<Entry<InboxKey<?>, InboxEntry<?>>> entrySet = inbox.entrySet();
+        for (Entry<InboxKey<?>, InboxEntry<?>> entry : entrySet) {
+            InboxEntry<?> value = entry.getValue();
+            out.println(entry.getKey() + " - "
+                    + "Expiry:" + new Date(value.getExpiry()) + " - "
+                    + "Destination:" + value.getMessage().getClass());
+        }
+    } catch (Exception e) {
+        out.println("Could not get outbox information");
+        e.printStackTrace(out);
+    }
+} finally {
+    Thread.currentThread().setContextClassLoader(oldContext);
+}
