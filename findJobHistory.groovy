a7bcc8f7c2217bed86b6a222fae4d2f95af5ed68
Joshua Shinn <jshinn@cloudbees.com>
2018-01-11 16:21:53 -0500

    Create findJobHistory.groovy

---
+++ findJobHistory.groovy
@@ -0,0 +1,24 @@
+//This script is intended to be run from a freestyle job as it may timeout from script console.  Freestyle job -> Execute system groovy script.  Imports are required.
+
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+
+def folder_name_pattern = /.*prod_deploy*/ //Folder name pattern, determines which folders to scan
+def jenkinsFolders = Jenkins.instance.getAllItems(com.cloudbees.hudson.plugins.folder.Folder)
+def matchedFolders = jenkinsFolders.findAll { folder ->
+    folder.name =~ folder_name_pattern
+}
+
+matchedFolders.each { folder ->
+  jobsList = folder.getAllJobs()
+    jobsList.each{ job ->
+      allBuilds = job.getBuilds()
+  	  allBuilds.each {
+      def cause = it.getCauses()[0]
+      def user = cause instanceof Cause.UserIdCause? cause.getUserId():""
+        println "Job Name: " + folder.name + "/" + job.name + " Build Number:" + it.number +  " Build Time: " + it.time + " Username: " + user
+  	  }
+    }
+}
