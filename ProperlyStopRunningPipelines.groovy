9a293aa8b7d8a0a0fa3abb3054f9c796a5a7a2bb
Alex Taylor <alexanderrtaylor@gmail.com>
2018-07-23 09:56:13 -0400

    Update ProperlyStopRunningPipelines.groovy
    Corrected getItemByFullName
    Corrected the list rather than doing a map which included indexes(better solution for sure)
    Added byTimestamp lookup because as long as start time is before the "delay" and the finish time is current it should show running builds older than the delayed time

--- ProperlyStopRunningPipelines.groovy
+++ ProperlyStopRunningPipelines.groovy
@@ -18,17 +18,17 @@ import groovy.time.*;
 // A couple of example map definitions:
 
 //Used to kill all of the active jobs
-//def jobMap = [:]
+//def jobMap = []
 
-//Used to clean up specific builds. Put "buildNum: 0" if you want to stop the latest build
+//Used to clean up specific builds. Put "buildNum: 0" if you want to stop all of the running builds for that job
 /*def jobMap = [
-    1: [name:'$JOB_NAME', buildNum: $BUILD_NUM],
-    2: [name:'$JOB_NAME2', buildNum: $BUILD_NUM2],
-  	3: [name:'$JOB_NAME3', buildNum: $BUILD_NUM3]
-]
-*/
+    //[name:'$JOB_NAME', buildNum: $BUILD_NUM2],
+    //[name:'$JOB_NAME2', buildNum: $BUILD_NUM2],
+  	//[name:'$JOB_NAME3', buildNum: $BUILD_NUM3]
+]*/
 
-def jobMap = [:]
+
+def jobMap = []
 
 //Created for if the map is empty to add all the jobs
 if(jobMap.isEmpty()){
@@ -36,7 +36,7 @@ if(jobMap.isEmpty()){
     job -> job.builds.findAll{it.isBuilding()}.each{
       build ->
       println("Adding: "+ job.fullName+ " build number " + build.getNumber().toInteger())
-      jobMap.put(++jobMap.size(), [ name: job.fullName, buildNum: build.getNumber().toInteger()])
+      jobMap.add([ name: job.fullName, buildNum: build.getNumber().toInteger()])
      }
    }
 }
@@ -44,24 +44,24 @@ use(TimeCategory)  {
   def delay = 2.days;//Put in a Custom date here to kill anything older
   // NO MODIFICATION
   Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).each{
-    job -> job.builds.findAll{it.isBuilding() && new Date(it.startTimeInMillis) < (new Date() - delay)}.each{
+    job -> job.builds.byTimestamp(refDate, new Date().time).each{
       build ->
       println("Adding: "+ job.fullName+ " build number " + build.getNumber().toInteger())
-      jobMap.put(++jobMap.size(), [ name: job.fullName, buildNum: build.getNumber().toInteger()])
+      jobMap.add([ name: job.fullName, buildNum: build.getNumber().toInteger()])
      }
    }
  }
 println "Removing the running builds for the following jobs: "
-for(int i=1; i<= jobMap.size(); i++)
+for(int i=0; i< jobMap.size(); i++)
 {
   def currentName = jobMap.get(i).name
-  def currentItem = Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).find{it.name.equals(currentName)}
+  def currentItem = Jenkins.instance.getItemByFullName(currentName)
   if (currentItem.isBuilding()){
     currentItem.builds.each{
       build ->
       if (build.isInProgress()&& jobMap.get(i).buildNum.equals(0)&& jobMap.get(i).name.equals(currentName)){
         println("Adding: "+ currentName+ " build number " + build.getNumber().toInteger())
-        jobMap.put(++jobMap.size(), [ name: currentName, buildNum: build.getNumber().toInteger()])
+      jobMap.add([ name: currentName, buildNum: build.getNumber().toInteger()])
       }
     }
     //Calling the same as the `X` in the UI
@@ -72,12 +72,12 @@ for(int i=1; i<= jobMap.size(); i++)
     }
   }
 }
-Thread.sleep(30000)
+//Thread.sleep(30000)
 //Calling the same as the Terminate running build command in the console log
-for(int i=1; i<= jobMap.size(); i++)
+for(int i=0; i< jobMap.size(); i++)
 {
   def currentName = jobMap.get(i).name
-  def currentItem = Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).find{it.name.equals(currentName)}
+  def currentItem = Jenkins.instance.getItemByFullName(currentName)
   if (currentItem.isBuilding()){
     def currentBuild = jobMap.get(i).buildNum
     if(currentBuild){
@@ -86,12 +86,12 @@ for(int i=1; i<= jobMap.size(); i++)
     }
   }
 }
-Thread.sleep(30000)
+//Thread.sleep(30000)
 //Calling the same as the Kill running build command in the console log
-for(int i=1; i<= jobMap.size(); i++)
+for(int i=0; i< jobMap.size(); i++)
 {
   def currentName = jobMap.get(i).name
-  def currentItem = Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).find{it.name.equals(currentName)}
+  def currentItem = Jenkins.instance.getItemByFullName(currentName)
   if (currentItem.isBuilding()){
     def currentBuild = jobMap.get(i).buildNum
     if(currentBuild){
