12b4755b9767120ca5ace426f13974fbff0c1fa3
Denys Digtiar <ddigtiar@cloudbees.com>
2018-10-09 15:07:37 +1000

    Update get-lastlogin-users.groovy
    The last login date is not actually last login date since it only changes when granted authorities change.

--- get-lastlogin-users.groovy
+++ get-lastlogin-users.groovy
@@ -1,6 +1,8 @@
 /**
 @Author kuisathaverat
-list all real users and shows the lastLogin datetime, those that have LastGrantedAuthoritiesProperty because them logen one time.
+Lists all "real" users and shows the timestamp. User that have `LastGrantedAuthoritiesProperty` are "real" because
+they loged in at least one time.
+WARNING: Timestamp is not the date of the last login. It is the last time granted authorities changed, e.g., user group.
 **/
 import org.acegisecurity.*
 import hudson.model.User
@@ -10,15 +12,16 @@ import java.util.Date
 User.getAll().each{ u ->
   def prop = u.getProperty(LastGrantedAuthoritiesProperty)
   def realUser = false
-  def lastLogin = null
+  def timestamp = null
   if (prop) {
     realUser=true
-    lastLogin = new Date(prop.timestamp).toString()
+    timestamp = new Date(prop.timestamp).toString()
   }
 
   if (realUser){
-    println u.getId() + ':' + u.getDisplayName() + ':Jenkins-User:' + u?.getDescription() + ':lastLogin=' + lastLogin
+    println u.getId() + ':' + u.getDisplayName() + ':Jenkins-User:' + u?.getDescription() + ':timestamp=' + timestamp
   } else if (realUser==false){
     println u.getId() + ':' + u.getDisplayName() + ':No-Jenkins-User:' + u?.getDescription()
   }
-} 
+}
+return
