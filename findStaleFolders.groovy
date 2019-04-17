3aa7e818d5675ac5ad081a2c8db35fe9316910c4
Jonathon Michael Golden <jgolden@cloudbees.com>
2019-04-17 16:44:44 -0700

    find folders where no jobs have run in the specified duration

---
+++ findStaleFolders.groovy
@@ -0,0 +1,26 @@
+import groovy.time.TimeCategory
+
+now = new Date()
+definitionOfStaleInDays = 100
+
+def jenkinsFolders = Jenkins.instance.getAllItems(com.cloudbees.hudson.plugins.folder.Folder)
+jenkinsFolders.each { folder ->
+  if(isStaleFolder(folder)){
+  	println "${folder.name} IS A STALE FOLDER"
+  }
+}
+
+boolean isStaleFolder(folder){
+	boolean returnValue
+    use(TimeCategory) {
+    	for(job in folder.getAllJobs()) {
+    		if (job.getLastBuild().getTime() > now - definitionOfStaleInDays.days) {
+     			returnValue = false
+     			break
+     		}else{
+     			returnValue = true
+     		}
+    	}
+    }
+    return returnValue
+}
