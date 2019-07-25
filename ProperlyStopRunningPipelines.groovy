ea27ebb734bfe78d3357ac74a07fef4cfa8d7fae
Allan Burdajewicz <allan.burdajewicz@gmail.com>
2019-07-25 11:29:47 +1000

    [CE-2935] Improve script for aborting running pipelines. (#100)

--- ProperlyStopRunningPipelines.groovy
+++ ProperlyStopRunningPipelines.groovy
@@ -1,108 +1,87 @@
+#!/bin/env groovy
+
+
 /*
-Author: Alex Taylor
-Since: July 2018
+Author: Alex Taylor, Allan Burdajewicz
+Since: July 2019
 Description: This script stop all or a series of running pipeline jobs
-reset or became out of synch.
 Parameters: None
 Scope: Cloudbees Jenkins Platform
 */
 
-/* This script can be used to kill off all running jobs, the latest build of a job, a specific job number, or jobs
-which have been running for a certain amount of time. This is all based on a couple of specific settings which are marked
-with comments. This script will guaranteed take 1.5 minutes to run because we want to ensure that each build command has the
-time needed to run*/
+/* This script can be used to kill off all running jobs which have been running for a certain amount of time. This 
+script will guaranteed take 30 seconds to run because we want to ensure that each build command has the time needed 
+to run. It is best to set Jenkins in a Quietdown (Shutdown) mode before running the script */
 
-import groovy.time.*;
+import groovy.time.TimeCategory
+import hudson.model.Queue
+import org.jenkinsci.plugins.workflow.job.WorkflowRun
+import org.jenkinsci.plugins.workflow.support.steps.ExecutorStepExecution
 
-
-// A couple of example map definitions:
-
-//Used to kill all of the active jobs
-//def jobMap = []
-
-//Used to clean up specific builds. Put "buildNum: 0" if you want to stop all of the running builds for that job
-/*def jobMap = [
-    //[name:'$JOB_NAME', buildNum: $BUILD_NUM2],
-    //[name:'$JOB_NAME2', buildNum: $BUILD_NUM2],
-  	//[name:'$JOB_NAME3', buildNum: $BUILD_NUM3]
-]*/
-
-
-def jobMap = []
-
-//Created for if the map is empty to add all the jobs
-if(jobMap.isEmpty()){
-  Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).each{
-    job -> job.builds.findAll{it.isBuilding()}.each{
-      build ->
-      println("Adding: "+ job.fullName+ " build number " + build.getNumber().toInteger())
-      jobMap.add([ name: job.fullName, buildNum: build.getNumber().toInteger()])
-     }
-   }
-}
-use(TimeCategory)  {
-  def delay = 2.days;//Put in a Custom date here to kill anything older
-  def refDate = (new Date()- delay).time
-  // NO MODIFICATION
-  Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).each{
-    job -> job.builds.byTimestamp(refDate, new Date().time).each{
-      build ->
-      println("Adding: "+ job.fullName+ " build number " + build.getNumber().toInteger())
-      jobMap.add([ name: job.fullName, buildNum: build.getNumber().toInteger()])
-     }
-   }
- }
-println "Removing the running builds for the following jobs: "
-for(int i=0; i< jobMap.size(); i++)
-{
-  def currentName = jobMap.get(i).name
-  def currentItem = Jenkins.instance.getItemByFullName(currentName)
-  if (currentItem.isBuilding()){
-    def currentBuild = jobMap.get(i).buildNum
-    currentItem.builds.each{
-      build ->
-      if (build.isInProgress()&& jobMap.get(i).buildNum.equals(0)&& jobMap.get(i).name.equals(currentName)){
-        println("Adding: "+ currentName+ " build number " + build.getNumber().toInteger())
-        jobMap.add([ name: currentName, buildNum: build.getNumber().toInteger()])
-      }
-      //Adding description for reason to abort builds forcefully
-       if (build.isInProgress()&& jobMap.get(i).buildNum.equals(currentBuild)&& jobMap.get(i).name.equals(currentName)){
-          build.setDescription("Build was suspended for taking too much time. Contact to your Jenkins administrators")
-          build.save()
-       }
+/**
+ * Try to infer the WorkflowRun of the executable passed in.
+ * @param executable The executable
+ * @return The WorkflowRun, or null if this is not a Pipeline run
+ */
+WorkflowRun getPipelineRunFromExecutable(Queue.Executable executable) {
+    if (executable instanceof WorkflowRun) {
+        return ((WorkflowRun) executable)
     }
-    //Calling the same as the `X` in the UI
-    if(currentBuild){
-        println("Stopping " + currentName + " Build Number "+ currentBuild);
-        Jenkins.instance.getItemByFullName(currentName).getBuildByNumber(currentBuild).doStop();
+
+    if (executable.parent instanceof ExecutorStepExecution.PlaceholderTask) {
+        def executorPlaceholderTask = ((ExecutorStepExecution.PlaceholderTask) executable.parent)
+        return ((WorkflowRun) executorPlaceholderTask.runForDisplay())
     }
-  }
+
+    return null
 }
-Thread.sleep(30000)
-//Calling the same as the Terminate running build command in the console log
-for(int i=0; i< jobMap.size(); i++)
-{
-  def currentName = jobMap.get(i).name
-  def currentItem = Jenkins.instance.getItemByFullName(currentName)
-  if (currentItem.isBuilding()){
-    def currentBuild = jobMap.get(i).buildNum
-    if(currentBuild){
-        println("Terminating " + currentName + " Build Number "+ currentBuild);
-        Jenkins.instance.getItemByFullName(currentName).getBuildByNumber(currentBuild).doTerm();
+
+def doForAllPipelineInProgress = { Closure closure ->
+    use(TimeCategory) {
+        def delay = 1.days
+        def processedPipeline = []
+        jenkins.model.Jenkins.instanceOrNull.getComputers().each { computer ->
+
+            computer.allExecutors.findAll { exec -> exec.isBusy() && exec.currentExecutable && exec.elapsedTime > delay.toMilliseconds() }.each { exec ->
+                def currentPipelineRun = getPipelineRunFromExecutable(exec.currentExecutable)
+                if (currentPipelineRun) {
+                    def pipelineRunId = currentPipelineRun.getExternalizableId()
+                    if(!processedPipeline.contains(pipelineRunId)) {
+                        processedPipeline.add(pipelineRunId)
+                        closure(exec, currentPipelineRun)
+                    } else {
+                        "   (Already processed ${currentPipelineRun})"
+                    }
+                }
+            }
+        }
     }
-  }
 }
-Thread.sleep(30000)
-//Calling the same as the Kill running build command in the console log
-for(int i=0; i< jobMap.size(); i++)
-{
-  def currentName = jobMap.get(i).name
-  def currentItem = Jenkins.instance.getItemByFullName(currentName)
-  if (currentItem.isBuilding()){
-    def currentBuild = jobMap.get(i).buildNum
-    if(currentBuild){
-        println("Killing " + currentName + " Build Number "+ currentBuild);
-        Jenkins.instance.getItemByFullName(currentName).getBuildByNumber(currentBuild).doKill();
+
+boolean somethingHappened = false
+doForAllPipelineInProgress { exec, run ->
+    println " * Stopping ${run.fullDisplayName} that spent ${exec.elapsedTime}ms building on ${exec.owner.displayName} #${exec.number}..."
+    run.doStop()
+    somethingHappened = true
+}
+
+if(somethingHappened) {
+    somethingHappened = false
+    sleep(30000)
+    doForAllPipelineInProgress { exec, run ->
+        println " * Forcibly Terminating ${run.fullDisplayName} that spent ${exec.elapsedTime}ms building on ${exec.owner.displayName} #${exec.number}..."
+        run.doTerm()
+        somethingHappened = true
     }
-  }
 }
+
+if(somethingHappened) {
+    somethingHappened = false
+    sleep(30000)
+    doForAllPipelineInProgress { exec, run ->
+        println " * Forcibly Killing ${run.fullDisplayName} that spent ${exec.elapsedTime}ms building on ${exec.owner.displayName} #${exec.number}..."
+        run.doKill()
+        somethingHappened = true
+    }
+}
+return
