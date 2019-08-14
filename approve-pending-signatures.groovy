d813be7cc28f0b0c98778ca109ba1f220533f088
MCardenasBlanco <cardenas.mcb@gmail.com>
2019-08-14 15:33:10 -0400

    CE-3195 Script to approve all the  pending signatures

---
+++ approve-pending-signatures.groovy
@@ -0,0 +1,37 @@
+/*
+Small script that can be used to approve all the pending signatures
+To be used with extreme caution
+*/
+
+import org.jenkinsci.plugins.scriptsecurity.scripts.*
+ScriptApproval sa = ScriptApproval.get();
+
+println "Signatures pending Approval..."
+for (ScriptApproval.PendingSignature pending : sa.getPendingSignatures()) {
+        println "Pending approval : " + pending.signature
+}
+
+
+println "Starting cleanup..."
+sa = ScriptApproval.get();
+
+
+
+while(sa.getPendingSignatures().size()>0) {
+  try{
+        item= sa.getPendingSignatures()[0]
+        println item.signature
+        sa.approveSignature(item.signature);
+        println "Approved : " + item.signature
+
+  }catch (Exception e){
+    println e
+  }
+}
+
+println "Verify that the signatures were approved..."
+sa = ScriptApproval.get();
+println "Retrieving pending approval signatures..."
+for (ScriptApproval.PendingSignature pending : sa.getPendingSignatures()) {
+        println "Pending approval : " + pending.signature
+}
