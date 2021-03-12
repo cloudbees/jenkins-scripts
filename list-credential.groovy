8edbd7f71fec61b69c6ae5e6e25280a48145126b
carlosrodlop <it.carlosrodlop@gmail.com>
2021-03-12 14:02:29 +0100

    [CE-5879]: addressing comments

--- list-credential.groovy
+++ list-credential.groovy
@@ -9,23 +9,21 @@ Test on CloudBees CI Cloud Operations Center 2.277.1.2
 import com.cloudbees.plugins.credentials.Credentials
 import com.cloudbees.plugins.credentials.CredentialsProvider
 
-Set<Credentials> allCredentials = new HashSet<Credentials>()
-
+Set<Credentials> allCredentials = new HashSet<Credentials>();
 Jenkins.instance.getAllItems(com.cloudbees.hudson.plugins.folder.Folder.class).each{ f ->
  creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
       com.cloudbees.plugins.credentials.Credentials.class, f)
   allCredentials.addAll(creds)
 
 }
-
 for (c in allCredentials) {
   if (CredentialsProvider.FINGERPRINT_ENABLED) {
     fp = CredentialsProvider.getFingerprintOf(c)
-    if (fp){  
+    if (fp){
     	fp_str="Fingerprinted jobs: " + fp.getJobs()
   	} else {
     	fp_str="(No Fingerprints)"
     }  
-  }  
+  }
   println(c.id + " : " + c.description  + " | " + fp_str)
-} 
+}
