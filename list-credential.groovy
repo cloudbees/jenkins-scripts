a140059a09d85484dbd1621bb7abc03708c07006
carlosrodlop <it.carlosrodlop@gmail.com>
2021-03-12 10:56:31 +0100

    CE-5879: adding test comments

--- list-credential.groovy
+++ list-credential.groovy
@@ -4,6 +4,7 @@ Description: List ID and Description of all credentials on a Jenkins Instance.
 It includes the fingerprints under the following considerations: 
    1) Job needs to have enable the Fingerprints. See https://www.jenkins.io/doc/book/using/fingerprints/ 
    2) Usage tracking requires the cooperation of plugins and consequently may not track every use.
+Test on CloudBees CI Cloud Operations Center 2.277.1.2
 **/
 import com.cloudbees.plugins.credentials.Credentials
 import com.cloudbees.plugins.credentials.CredentialsProvider
