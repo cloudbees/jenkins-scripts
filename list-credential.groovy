b6f0d509cb39b7c9768304a05c54b6fb89cb0766
Carlos Rodriguez Lopez <crodriguezlopez@cloudbees.com>
2021-03-15 12:15:43 +0100

    CE-5879: adding fingerprinting to the credential list (#173)
    * CE-5879: adding fingerprinting
    
    * CE-5879: adding test comments
    
    * Update list-credential.groovy
    
    Co-authored-by: Felix Belzunce Arcos <fbelzunc@gmail.com>
    
    * Update list-credential.groovy
    
    Co-authored-by: Felix Belzunce Arcos <fbelzunc@gmail.com>
    
    * Update list-credential.groovy
    
    Co-authored-by: Felix Belzunce Arcos <fbelzunc@gmail.com>
    
    * [CE-5879]: addressing comments
    
    Co-authored-by: Felix Belzunce Arcos <fbelzunc@gmail.com>

--- list-credential.groovy
+++ list-credential.groovy
@@ -1,24 +1,29 @@
 /**
-Author: kuisathaverat
+Author: kuisathaverat, carlosrodlop
 Description: List ID and Description of all credentials on a Jenkins Instance.
+It includes the fingerprints under the following considerations: 
+   1) Job needs to have enable the Fingerprints. See https://www.jenkins.io/doc/book/using/fingerprints/ 
+   2) Usage tracking requires the cooperation of plugins and consequently may not track every use.
+Test on CloudBees CI Cloud Operations Center 2.277.1.2
 **/
 import com.cloudbees.plugins.credentials.Credentials
+import com.cloudbees.plugins.credentials.CredentialsProvider
 
 Set<Credentials> allCredentials = new HashSet<Credentials>();
-
-def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-      com.cloudbees.plugins.credentials.Credentials.class
-);
-
-allCredentials.addAll(creds)
-
 Jenkins.instance.getAllItems(com.cloudbees.hudson.plugins.folder.Folder.class).each{ f ->
  creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
       com.cloudbees.plugins.credentials.Credentials.class, f)
   allCredentials.addAll(creds)
-  
-}
 
+}
 for (c in allCredentials) {
-   println(c.id + ": " + c.description)
+  if (CredentialsProvider.FINGERPRINT_ENABLED) {
+    fp = CredentialsProvider.getFingerprintOf(c)
+    if (fp) {  
+    	fp_str = "Fingerprinted jobs: " + fp.getJobs()
+  	} else {
+    	fp_str = "(No Fingerprints)"
+    }  
+  }
+  println(c.id + " : " + c.description  + " | " + fp_str)
 }
