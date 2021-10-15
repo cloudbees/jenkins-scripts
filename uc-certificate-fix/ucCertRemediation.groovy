b28ad7d463c180cf9e4afb1d43ecebf8523a139c
Ray Kivisto <ray.kivisto@gmail.com>
2021-10-15 17:40:41 -0400

    fix spelling of successful

--- uc-certificate-fix/ucCertRemediation.groovy
+++ uc-certificate-fix/ucCertRemediation.groovy
@@ -351,7 +351,7 @@ if (result.equals("NO_CHANGE_NEEDED")) {
     writeScriptToInitGroovyFolder(_script);
     println("Reloading update center data");
     Jenkins.getInstance().pluginManager.doCheckUpdatesServer();
-    println("The remediation is now complete and sucessful");
+    println("The remediation is now complete and successful");
 } else if (result.equals("UNINSTALLED_SCRIPT")) {
     println("No issues detected, script has been uninstalled");
 } else {
