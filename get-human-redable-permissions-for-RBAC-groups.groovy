64f60c2fe5d7aa0754b1e1c832bb7a71e35ddf75
KevinRealAlcalde <krealalcalde@cloudbees.com>
2022-07-12 17:58:23 +0100

    get human readable permissions for RBAC groups

---
+++ get-human-redable-permissions-for-RBAC-groups.groovy
@@ -0,0 +1,32 @@
+import nectar.plugins.rbac.strategy.RoleMatrixAuthorizationStrategyImpl;
+import nectar.plugins.rbac.strategy.RoleMatrixAuthorizationConfig;
+import nectar.plugins.rbac.strategy.RoleMatrixAuthorizationPlugin;
+import hudson.security.PermissionGroup;
+import nectar.plugins.rbac.roles.Role;
+
+//Obtain security configuration
+RoleMatrixAuthorizationStrategyImpl strategy = RoleMatrixAuthorizationStrategyImpl.getInstance()
+RoleMatrixAuthorizationConfig config = RoleMatrixAuthorizationPlugin.getConfig()
+
+def map = [:]
+
+PermissionGroup.getAll().each { permissionGroup ->
+  permissionGroup.permissions.each { permission ->
+    if (permission.enabled && permissionGroup.id != "N/A") {
+      map[permission.id] = permissionGroup.id + '/' + permission.name
+    }
+  }
+}
+
+config.getRoles().each{r ->
+    DESCRIPTION = r
+    // println '\t' + r
+    // println '\t\t Role Permissions'
+    Role rc = new Role(r)
+
+    rc.getPermissionProxies().each{p ->
+        println DESCRIPTION + ' - ' + map[p.id]
+    }
+}
+
+return null
