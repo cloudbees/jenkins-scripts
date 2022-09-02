8559756b584e0017529068a33e2f09ed171280ba
KevinRealAlcalde <95503519+KevinRealAlcalde@users.noreply.github.com>
2022-09-02 15:52:06 +0100

    Taking Allan proposal of using Role information instead of mapping
    Co-authored-by: Allan Burdajewicz <aburdajewicz@cloudbees.com>

--- get-human-redable-permissions-for-RBAC-groups.groovy
+++ get-human-redable-permissions-for-RBAC-groups.groovy
@@ -1,30 +1,10 @@
-import nectar.plugins.rbac.strategy.RoleMatrixAuthorizationStrategyImpl;
 import nectar.plugins.rbac.strategy.RoleMatrixAuthorizationConfig;
 import nectar.plugins.rbac.strategy.RoleMatrixAuthorizationPlugin;
-import hudson.security.PermissionGroup;
 import nectar.plugins.rbac.roles.Role;
 
-//Obtain security configuration
-RoleMatrixAuthorizationStrategyImpl strategy = RoleMatrixAuthorizationStrategyImpl.getInstance()
-RoleMatrixAuthorizationConfig config = RoleMatrixAuthorizationPlugin.getConfig()
-
-def map = [:]
-
-//Populate map
-PermissionGroup.getAll().each { permissionGroup ->
-  permissionGroup.permissions.each { permission ->
-    if (permission.enabled && permissionGroup.id != "N/A") {
-      map[permission.id] = permissionGroup.id + '/' + permission.name
-    }
-  }
-}
-
-config.getRoles().each{r ->
-    DESCRIPTION = r
-    Role rc = new Role(r)
-
-    rc.getPermissionProxies().each{p ->
-        println DESCRIPTION + ' - ' + map[p.id]
+RoleMatrixAuthorizationPlugin.getConfig().getRoles().each{r ->
+    new Role(r).getPermissionProxies().each{p ->
+        println r + " - " + p.group.title + '/' + p.name
     }
 }
 
