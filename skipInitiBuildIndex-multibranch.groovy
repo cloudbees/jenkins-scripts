e0b16d8ed048809d2c51bb7d474f3efa4b3c7340
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-11-11 17:08:11 +0100

    Create skipInitiBuildIndex-multibranch.groovy

---
+++ skipInitiBuildIndex-multibranch.groovy
@@ -0,0 +1,47 @@
+/**
+ * This script is used to enable the build strategy SkipInitialBuildOnFirstIndexingResetRevision for all multibranch projects.
+ * Use Case: Migration of Jobs across CloudBees CI instances to prevent Build Storm.
+ * Requirement: https://docs.cloudbees.com/docs/release-notes/latest/plugins/cloudbees-build-strategies-plugin/ 
+ */
+
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
+import com.cloudbees.jenkins.plugins.buildstrategies.SkipInitialBuildOnFirstIndexingResetRevision
+
+def dryRun = true
+
+
+def enableBuildStrategy(dryRun) {
+    def modifiedCount = 0
+    def emptyCount = 0
+    def nonEmptyCount = 0
+
+    Jenkins.instance.getAllItems(WorkflowMultiBranchProject).each { multibranchProject ->
+		
+        multibranchProject.getSourcesList().each { branchSource ->
+            if (branchSource !=null) { 
+                def buildStrategies = branchSource.getBuildStrategies()
+                if (buildStrategies.isEmpty()) {
+                    if (dryRun){
+                        println "Dryrun: ${multibranchProject.fullName} is a candidate to add Build Strategy."
+                    } else {
+                        branchSource.setBuildStrategies(Arrays.asList(new SkipInitialBuildOnFirstIndexingResetRevision()))
+                        println "Adding Build Strategy SkipInitialBuildOnFirstIndexingResetRevision for ${multibranchProject.fullName}" 
+                        modifiedCount++
+                    }
+                    emptyCount++
+                } else {
+                    println "${multibranchProject.fullName} has already enabled Build Strategy ${buildStrategies}."
+                    nonEmptyCount++
+                }
+            }
+        }
+    }
+    println "===================================================================================="
+    println "Non-empty build strategies count: ${nonEmptyCount}"
+    println "Empty build strategies count: ${emptyCount}"
+    println "Added build strategies SkipInitialBuildOnFirstIndexingResetRevision: ${modifiedCount}"
+    println "===================================================================================="
+}
+
+enableBuildStrategy(dryRun)
+null
